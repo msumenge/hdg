00a6334af9e58d6a8eb70844be2317b6764bac80
msumenge <110424@student.sae.edu.au>
2016-02-10 15:29:45 +1000

    Contact form - back end

--- application/controllers/contact.php
+++ application/controllers/contact.php
@@ -10,4 +10,75 @@ class Contact extends CI_Controller {
 		$this->load->view('contact');
 		$this->load->view('body_end');
 	}
+    
+    function submit() {
+        $name = $this->input->post('contact-name');
+        $email = $this->input->post('contact-email');
+        $phone = $this->input->post('contact-phone');
+        $message = $this->input->post('contact-message');
+        
+        if(valid_email($email)) {
+            $this->load->library('email');
+
+            $config = Array(
+                'protocol' => 'mail',
+                'mailtype'  => 'html', 
+                'charset'   => 'iso-8859-1',
+                'wordwrap'	=> TRUE
+            );
+            
+            //****************************************************************
+            //*************** EDIT *******************************************
+            //****************************************************************
+            
+                    $config['smtp_host'] = 'mail.handsomedragongames.com';
+                    $config['smtp_port'] = 25;
+                    $config['smtp_user'] = 'contact@handsomedragongames.com';
+                    $config['smtp_pass'] = 'handsomeDragon123';
+            
+            //****************************************************************
+            //****************************************************************
+            //****************************************************************
+            
+            $this->email->initialize($config);
+            $this->email->set_newline("\r\n");
+
+            $this->email->from($email, ucwords($name));
+            $this->email->to('msumenge@hotmail.com'); #admin@handsomedragongames.com	
+
+            $this->email->subject('Handsome Dragon Games contact form');
+            $this->email->message(
+                $message.'<br/><br/>'.
+                ucwords($name).'<br/>'.
+                $phone
+            );
+
+            if($this->email->send())
+                $this->session->set_flashdata('success', true);
+            else {
+                $this->session->set_flashdata('error', '<strong>Error!</strong> The message could not be sent. Please try again.');
+                $form_data = array(
+                    'name' => $name,
+                    'email' => $email,
+                    'phone' => empty($phone) ? '' : $phone,
+                    'message' => $message,
+                );
+                $this->session->set_flashdata('form_data', $form_data);
+            }
+        }
+        else {
+            $this->session->set_flashdata('error', '<strong>Error!</strong> Invalid email address. Please try again with a different email address.');
+            $form_data = array(
+                'name' => $name,
+                'email' => $email,
+                'phone' => empty($phone) ? '' : $phone,
+                'message' => $message,
+            );
+            $this->session->set_flashdata('form_data', $form_data);
+        }
+        
+        //return to contact page
+        header('location: ' . base_url('contact'));
+        exit;
+    }
 }
--- application/views/contact.php
+++ application/views/contact.php
@@ -5,28 +5,52 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-6">
-            <div class="col-md-12 marg-top-25">
-                <h4>Full name *</h4>
-                <input type="text" class="form-control" name="contact-name" required />
-            </div>
-            <div class="col-md-12 marg-top-15">
-                <h4>Email address *</h4>
-                <input type="email" class="form-control" name="contact-email" required />
-            </div>
-            <div class="col-md-12 marg-top-15">
-                <h4>Phone</h4>
-                <input type="text" class="form-control" name="contact-phone" />
+        <div class="col-md-12 marg-top-25">
+            <div class="col-md-12">
+                <?php
+                    if(isset($_SESSION['error'])) {
+                ?>
+                <div class="alert alert-danger alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <?php echo $_SESSION['error']; ?>
+                </div>
+                <?php
+                    }
+                    if(isset($_SESSION['success'])) {
+                ?>
+                <div class="alert alert-success alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    Your message has been sent successfully. <strong>Thank you!</strong>
+                </div>
+                <?php
+                    }
+                ?>
             </div>
         </div>
-        <div class="col-md-6">
-            <div class="col-md-12 marg-top-25">
-                <h4>Message *</h4>
-                <textarea class="fullwidth" style="height: 155px; padding: 6px 12px; border: 1px solid #ccc; max-width: 100%;" placeholder="Message..." name="contact-message" required></textarea>
+        <form action="<?php echo base_url('contact/submit'); ?>" method="post">
+            <div class="col-md-6">
+                <div class="col-md-12">
+                    <h4>Full name *</h4>
+                    <input type="text" class="form-control" name="contact-name" required value="<?php echo $form = isset($_SESSION['form_data']) ? $_SESSION['form_data']['name'] : ''; ?>"/>
+                </div>
+                <div class="col-md-12 marg-top-15">
+                    <h4>Email address *</h4>
+                    <input type="email" class="form-control" name="contact-email" required value="<?php echo $form = isset($_SESSION['form_data']) ? $_SESSION['form_data']['email'] : ''; ?>"/>
+                </div>
+                <div class="col-md-12 marg-top-15">
+                    <h4>Phone</h4>
+                    <input type="text" class="form-control" name="contact-phone" value="<?php echo $form = isset($_SESSION['form_data']) ? $_SESSION['form_data']['phone'] : ''; ?>"/>
+                </div>
             </div>
-            <div class="col-md-12 marg-top-15">
-                <input type="submit" class="btn btn-primary fullwidth" value="Submit">
+            <div class="col-md-6">
+                <div class="col-md-12">
+                    <h4>Message *</h4>
+                    <textarea class="fullwidth" style="height: 155px; padding: 6px 12px; border: 1px solid #ccc; max-width: 100%;" placeholder="Message..." name="contact-message" required><?php echo $form = isset($_SESSION['form_data']) ? $_SESSION['form_data']['message'] : ''; ?></textarea>
+                </div>
+                <div class="col-md-12 marg-top-15">
+                    <input type="submit" class="btn btn-primary fullwidth" value="Submit">
+                </div>
             </div>
-        </div>
+        </form>
     </div>
 </div>
